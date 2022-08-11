c31523a063e52b4218d8bdcc9cf84ccc5163e97a
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-11 10:56:50 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,14 @@
 export type Color = {
+  hue: number;
+  lightness: number;
   name: string;
   hex: string;
-  hsl: number[];
   rgb: number[];
+  isLocked: boolean;
 };
+
+export type Palette = {
+  saturation: number;
+  colors: Color[];
+  mode: number;
+}
