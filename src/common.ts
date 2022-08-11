b06f8eb0c34d9b2f2ef26f7d1e0354d04d319560
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-12 00:23:12 +0200

    Create common.ts

---
+++ src/common.ts
@@ -0,0 +1,5 @@
+export const newHue = (): number => Math.round(Math.random() * 360);
+
+export const newSaturation = (): number => Math.round(Math.random() * 100);
+
+export const newLightness = (): number => Math.round(Math.random() * 50) + Math.round(Math.random() * 50);
