29266ea7ddc2c168c2bbcefb58b4d59ddf8ac706
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-12 02:56:01 +0200

    Delete common.ts

--- src/common.ts
+++
@@ -1,5 +0,0 @@
-export const newHue = (): number => Math.round(Math.random() * 360);
-
-export const newSaturation = (): number => Math.round(Math.random() * 100);
-
-export const newLightness = (): number => Math.round(Math.random() * 50) + Math.round(Math.random() * 50);
