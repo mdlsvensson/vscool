9c27847a3bf4fa0c6af20e329b586f27316ddcf0
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-12 05:33:22 +0200

    Color params and default values

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,13 @@
 import { writable } from 'svelte/store';
-import type { Palette } from './types';
+import type { Palette, ColorParams } from './types';
 
 export const currentPalette = writable<Palette>();
 
-export const mode = writable<number>(0);
+export const mode = writable<number>(1);
 
-export const colorCount = writable<number>(0);
+export const colorCount = writable<number>(5);
+
+export const colorParams = writable<ColorParams>({
+  distance: 10,
+  direction: null,
+});
