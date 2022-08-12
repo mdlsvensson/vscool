9ab44cd9d0b5961bac1637d7179375838d2e99d3
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-12 05:33:26 +0200

    Create Button.svelte

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { mode } from '../stores';
+
+	export let value: number;
+</script>
+
+<button
+	class="bg-blue-300 py-1 px-3 rounded-md hover:bg-red-300 transition"
+	on:click={() => ($mode = value)}
+	class:bg-red-300={$mode === value}
+>
+	<slot />
+</button>
