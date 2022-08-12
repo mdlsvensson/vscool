264058737f358fc516537904b3390806f2376b28
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-12 02:56:07 +0200

    Create common.ts

---
+++ src/lib/common.ts
@@ -0,0 +1,9 @@
+import type { HSL } from "color-convert/conversions";
+
+export const newHue = (): number => Math.round(Math.random() * 360);
+
+export const newSaturation = (): number => Math.round(Math.random() * 100);
+
+export const newLightness = (): number => Math.round(Math.random() * 50) + Math.round(Math.random() * 50);
+
+export const newHsl = (): HSL => [newHue(), newSaturation(), newLightness()];
